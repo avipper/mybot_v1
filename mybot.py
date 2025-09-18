0cb8810800775b96718c5b388af5ddc78f2cea85
avipper <al.vl.petrov@gmail.com>
2025-09-18 15:12:59 +0300

    Update mybot.py
    изменение получения переменных окружения

--- mybot.py
+++ mybot.py
@@ -1,11 +1,15 @@
-# bot.py
 import os
 from aiohttp import web
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils.executor import start_webhook
 
-API_TOKEN = os.getenv("8380395268:AAHcRYxmenMO8WHfgJcC8Y-dESeZgwxrUYY")  # токен от BotFather
-WEBHOOK_HOST = os.getenv("https://mybot-v1-2u8v.onrender.com")  # URL Render, например https://my-tg-bot.onrender.com
+API_TOKEN = os.getenv("BOT_TOKEN") # Получение токена из переменных окружения
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST") # URL Render, например https://my-tg-bot.onrender.com
+
+# Проверка наличия токена
+if not API_TOKEN:
+raise ValueError("Переменная окружения BOT_TOKEN не установлена! Установите токен от BotFather.")
+
 WEBHOOK_PATH = f"/webhook/{API_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
@@ -41,3 +45,8 @@ app.router.add_post(WEBHOOK_PATH, handle)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     web.run_app(app, host="0.0.0.0", port=port)
+
+
+
+
+
